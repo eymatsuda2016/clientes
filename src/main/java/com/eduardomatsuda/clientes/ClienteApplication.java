e5286438bde3738fc94fc58448f9b575feab0be6
eymatsuda2016 <eymatsuda@2016@outlook.com>
2024-04-14 22:19:59 -0300

    Instalado e Configurado o Lombok

--- src/main/java/com/eduardomatsuda/clientes/ClienteApplication.java
+++ src/main/java/com/eduardomatsuda/clientes/ClienteApplication.java
@@ -1,13 +1,14 @@
 package com.eduardomatsuda.clientes;
 
+import lombok.Data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Data
 public class ClienteApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ClienteApplication.class, args);
     }
-
 }
